6af83e289e0684c76c5ccf8947970e8670d6529e
aspyn-aa <aspyn.aa@kakaocorp.com>
2024-07-28 00:07:44 +0900

    feat: 헤더 추가

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ProfileImage } from "../ui/upload/profile-image";
+
+const Page = () => {
+  return (
+    <div>
+      <ProfileImage profileImageUrl="https://p.kakaocdn.net/th/talkp/woKr823m3V/6wf5l3pA5SvFhSAjS53Zt1/rx7k78_110x110_c.jpg" />
+    </div>
+  );
+};
+
+export default Page;
