71346ae68541a5d5cbc6f8c552c28c4ce54f98a6
ksj12172 <sojeong94@naver.com>
2024-08-30 16:17:31 +0900

    feat: 설문조사 리스트 렌더 추가

--- src/app/poll/page.tsx
+++ src/app/poll/page.tsx
@@ -1,13 +1,31 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import {FaCircleCheck} from 'react-icons/fa6';
+import {IoIosArrowForward} from 'react-icons/io';
+import {IoPeopleOutline} from 'react-icons/io5';
+import {MdHowToVote} from 'react-icons/md';
 import {NavBarHeight} from '../../../const';
 import pollImage from '../../../public/polling.png';
 
-const mockFeedList = Array.from({length: 10}, (_, i) => ({
+interface PollItem {
+  id: number;
+  title: string;
+  voteCount: number; // 몇 명이 투표에 참여했는지
+  hasVoted: boolean; // 유저가 투표에 참여했는지
+  image: string | null; // 투표 만들 때 이미지 등록 안 했으면 디폴트 이미지 노출
+}
+const mockPollList = Array.from({length: 10}, (_, i) => ({
   id: i,
-  title: '글 제목 글 제목 글 제목 글 제목 글 제목',
-  description:
-    '글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용',
-  image: 'https://cdn.usegalileo.ai/stability/31bdc9b2-f116-4477-a8a5-db1689be29ed.png',
+  title:
+    i % 2
+      ? '최애 슈즈 투표'
+      : '여러분의 최애 발레 슈즈를 투표해주세요. 길게길게testesteststestetstsettetestestesteststestetstsettetes',
+  voteCount: 3,
+  hasVoted: Boolean(i % 2),
+  image:
+    i % 2
+      ? null
+      : 'https://zjnkgnavmphkyf5n.public.blob.vercel-storage.com/nihal-demirci-erenay-UYG1U5wj3Tk-unsplash-RKVLJAGugUPwH0o5x4eWvNUCq2Q9PX.jpg',
 }));
 
 const page = () => {
@@ -15,28 +33,51 @@ const page = () => {
 
   return (
     <div className="py-[16px] px-2">
-      <div className="w-full flex items-end">
+      {/* 설문 조사 타이틀 */}
+      <div className="w-full flex items-end ml-[4px]">
         <Image src={pollImage} alt="설문 조사" width="45" height="45" />
         <h1 className="pl-[8px] text-lg">설문조사</h1>
       </div>
+      {/* 설문 조사 리스트 */}
       <div className="flex flex-col">
-        {mockFeedList.map(({id, title, description, image}) => (
-          <div className="flex gap-4 border-[1px] border-gray-200 p-4 mt-4 rounded-[8px]" key={id}>
-            <div className="w-2/3 flex flex-col">
-              <p className="text-[#0e141b] text-base font-bold leading-tight mb-2">{title}</p>
-              <p className="text-[#4e7397] text-sm font-normal leading-normal">
-                {description.replace(/\n/g, '').slice(0, 50)}...
-              </p>
+        {mockPollList.map(({id, title, voteCount, hasVoted, image}) => (
+          <Link href={'/poll/' + id} className="border-[1px] border-gray-200 p-4 mt-4 rounded-[8px]" key={id}>
+            <div className="flex flex-row w-full items-center">
+              <div className="w-[60px] h-[60px] overflow-hidden flex justify-center items-center rounded-full bg-[#e6e7e9]">
+                {image ? (
+                  <Image src={image} alt="투표 이미지" width="60" height="60" className="w-full h-full object-cover" />
+                ) : (
+                  <MdHowToVote className="w-[30px] h-auto" />
+                )}
+              </div>
+              <div className="flex flex-col ml-[8px] max-w-[calc(100%-100px)]">
+                <p className="text-[#0e141b] text-base overflow-hidden font-bold leading-tight mb-2 whitespace-nowrap text-ellipsis">
+                  {title}
+                </p>
+                <div className="flex flex-row text-[#4e7397] text-sm font-normal leading-normal">
+                  <div className="flex flex-row">
+                    <IoPeopleOutline size="20" className="mr-[4px]" />
+                    {voteCount}명 참여
+                  </div>
+                  {hasVoted && (
+                    <>
+                      &nbsp;
+                      <div>
+                        <FaCircleCheck size="20" color="green" className="ml-[8px]" />
+                        {hasVoted}
+                      </div>
+                    </>
+                  )}
+                </div>
+              </div>
+              <div className="ml-auto">
+                <IoIosArrowForward className="ml-[16px]" />
+              </div>
             </div>
-            <div
-              className="w-1/3 bg-center bg-no-repeat aspect-video bg-cover rounded-xl flex-1"
-              style={{
-                backgroundImage: `url("${image}")`,
-              }}
-            ></div>
-          </div>
+          </Link>
         ))}
       </div>
+      {/* 투표 추가하기 버튼 */}
       <div style={{bottom: bottomPosition}} className={`fixed w-full h-[72px] p-[16px]`}>
         <button className="bg-buttonColor text-white">+ 투표 추가</button>
       </div>
