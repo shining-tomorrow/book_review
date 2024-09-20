515386faa79b5419c193a5067cf896af981dc805
akh9804 <akh9804@gmail.com>
2024-09-20 13:50:28 +0900

    feat: 게시판 디자인 변경 (#13)

---
+++ src/ui/home/Feed.tsx
@@ -0,0 +1,67 @@
+import {DateTime} from 'luxon';
+import {useMemo} from 'react';
+
+interface Props {
+  feed: {
+    id: number;
+    title: string;
+    description: string;
+    date: string;
+    category: string;
+    views: number;
+    image?: string;
+  };
+}
+
+export default function Feed({feed}: Props) {
+  const parsedDescription = useMemo(() => feed.description.replace(/\n/g, ''), [feed.description]);
+  const parsedDate = useMemo(() => {
+    const today = DateTime.local();
+    const dateTime = DateTime.fromFormat(feed.date, 'yyyy-MM-dd');
+    const diff = today.diff(dateTime);
+
+    if (diff.as('second') < 60) {
+      return `${diff.as('second').toFixed()}초 전`;
+    }
+
+    if (diff.as('minute') < 60) {
+      return `${diff.as('minute').toFixed()}분 전`;
+    }
+
+    if (diff.as('hour') < 24) {
+      return `${diff.as('hour').toFixed()}시간 전`;
+    }
+
+    if (diff.as('day') < 7) {
+      return `${diff.as('day').toFixed()}일 전`;
+    }
+
+    if (diff.as('day') <= 31) {
+      return `${diff.as('week').toFixed()}주 전`;
+    }
+
+    if (diff.as('month') < 12) {
+      return `${diff.as('month').toFixed()}달 전`;
+    }
+
+    return `${diff.as('year').toFixed()}년 전`;
+  }, [feed.date]);
+
+  return (
+    <div className="p-2 flex flex-col gap-3 shadow rounded-xl">
+      <div className="bg-gray-100 py-1 px-2 rounded-full w-fit text-gray-500 text-sm">{feed.category}</div>
+      <div className="flex flex-col gap-1">
+        <h1 className="text-base font-semibold text-black">{feed.title}</h1>
+        <p className="text-gray-500 text-sm">
+          {parsedDescription.slice(0, 50)}
+          {parsedDescription.length > 50 && '...'}
+        </p>
+      </div>
+      <div className="flex items-center gap-1">
+        <time className="text-gray-500 text-sm">{parsedDate}</time>
+        <span className="text-gray-500 text-sm">·</span>
+        <span className="text-gray-500 text-sm">조회 {feed.views.toLocaleString()}</span>
+      </div>
+    </div>
+  );
+}
--- src/ui/home/FeedList.tsx
+++ src/ui/home/FeedList.tsx
@@ -1,30 +1,49 @@
-const mockFeedList = Array.from({length: 10}, (_, i) => ({
-  id: i,
-  title: '글 제목 글 제목 글 제목 글 제목 글 제목',
-  description:
-    '글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용 글 내용',
-  image: 'https://cdn.usegalileo.ai/stability/31bdc9b2-f116-4477-a8a5-db1689be29ed.png',
-}));
+import Feed from './Feed';
+
+const mockedFeedList = [
+  {
+    id: 1,
+    title: '글 제목',
+    description: '짧은 글 내용',
+    date: '2024-09-12',
+    category: '질문',
+    views: 200,
+    image:
+      'https://images.unsplash.com/photo-1726587912121-ea21fcc57ff8?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3fHx8ZW58MHx8fHx8',
+  },
+  {
+    id: 1,
+    title: '글 제목',
+    description:
+      '긴 글 내용 긴 글 내용 긴 글 내용 긴 글 내용 긴 글 내용 긴 글 내용 긴 글 내용 긴 글 내용 긴 글 내용 긴 글 내용 긴 글 내용 긴 글 내용 긴 글 내용 긴 글 내용 긴 글 내용 긴 글 내용',
+    date: '2024-09-16',
+    category: '꿀팁',
+    views: 1000,
+    image:
+      'https://images.unsplash.com/photo-1726587912121-ea21fcc57ff8?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3fHx8ZW58MHx8fHx8',
+  },
+];
+
+const categoryList = ['전체', '질문', '꿀팁', '수다', '정보', '공지', '자유', '취미', '공유'];
 
 export default function FeedList() {
   return (
-    <div className="p-2 flex flex-col">
-      {mockFeedList.map(({id, title, description, image}) => (
-        <div className="flex gap-4 border-[1px] border-gray-200 p-4 mt-4 rounded-[8px]" key={id}>
-          <div className="w-2/3 flex flex-col">
-            <p className="text-[#0e141b] text-base font-bold leading-tight mb-2">{title}</p>
-            <p className="text-[#4e7397] text-sm font-normal leading-normal">
-              {description.replace(/\n/g, '').slice(0, 50)}...
-            </p>
-          </div>
+    <div className="flex flex-col gap-4 py-4">
+      <div className="text-black flex items-center gap-2 overflow-x-auto hide-scrollbar">
+        {categoryList.map((category, idx) => (
           <div
-            className="w-1/3 bg-center bg-no-repeat aspect-video bg-cover rounded-xl flex-1"
-            style={{
-              backgroundImage: `url("${image}")`,
-            }}
-          ></div>
-        </div>
-      ))}
+            key={idx}
+            className={`shrink-0 border border-gray-500 py-2 px-3 rounded-full text-sm ${idx === 0 ? 'bg-gray-500 text-white' : 'bg-white text-gray-500'}`}
+          >
+            {category}
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-col gap-2">
+        {mockedFeedList.map(feed => (
+          <Feed key={feed.id} feed={feed} />
+        ))}
+      </div>
     </div>
   );
 }
