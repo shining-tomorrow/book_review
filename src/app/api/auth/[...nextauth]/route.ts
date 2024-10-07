6665660b634ba0013218aa4c9d762c42dab74e95
ksj12172 <sojeong94@naver.com>
2024-10-07 16:54:52 +0900

    fix: 로그인을 위한 secret 값을 명시적으로 설정

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -11,6 +11,7 @@ export const OPTIONS = {
     strategy: 'jwt' as SessionStrategy,
     maxAge: 60 * 60 * 1,
   },
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
