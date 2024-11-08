1db4a8f55ef314ebe6f6a5e3c03cea1d9e1d2061
ksj12172 <sojeong94@naver.com>
2024-11-08 13:32:19 +0900

    feat: 투표 폼 추가 작업

--- src/app/poll/new/page.tsx
+++ src/app/poll/new/page.tsx
@@ -1,15 +1,29 @@
 'use client';
 
 import {PutBlobResult} from '@vercel/blob';
+import {DateTime} from 'luxon';
 import Image from 'next/image';
 import {FormEvent, useState} from 'react';
 import {DefaultImage} from '../../../../const';
 
 const Page = () => {
   const [blob, setBlob] = useState<PutBlobResult | null>(null);
+  const [hasEndDate, setHasEndDate] = useState(false);
+  const [isAllowMultipleChoice, setIsAllowMultipleChoice] = useState(false);
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+  const today = DateTime.now().toISODate(); // 2024-11-08
+
+  const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement);
+    const formDataArray = Array.from(formData.entries());
+
+    const formValues = {};
+
+    formDataArray.forEach(([key, value]) => {
+      console.log(key, value);
+    });
   };
 
   const handleChange = async (event: any) => {
@@ -35,10 +49,32 @@ const Page = () => {
     setBlob(newBlob);
   };
 
+  const handleChangeHasEndDate = ({target}: any) => {
+    switch (target.id) {
+      case 'has_end_date':
+        setHasEndDate(target.checked);
+        break;
+      case 'not_has_end_date':
+        setHasEndDate(!target.checked);
+        break;
+    }
+  };
+
+  const handleChangeIsAllowMultipleChoice = ({target}: any) => {
+    switch (target.id) {
+      case 'allow_multiple_choice':
+        setIsAllowMultipleChoice(target.checked);
+        break;
+      case 'not_allow_multiple_choice':
+        setIsAllowMultipleChoice(!target.checked);
+        break;
+    }
+  };
+
   return (
     <form className="flex flex-col items-center justify-center w-[85%] mt-[10px]" onSubmit={handleSubmit}>
-      <div className="flex w-full">
-        <label className="w-[120px]" htmlFor="title">
+      <div className="flex w-full pt-[6px]">
+        <label className="w-[150px]" htmlFor="title">
           투표 제목
         </label>
         <input
@@ -50,8 +86,8 @@ const Page = () => {
         ></input>
       </div>
 
-      <div className="flex w-full">
-        <div className="w-[120px]">투표 썸네일 (선택)</div>
+      <div className="flex w-full pt-[6px]">
+        <div className="w-[150px]">투표 썸네일 (선택)</div>
         <div className="flex flex-grow">
           {/* TODO: 디폴트 이미지 넣기 */}
           <Image
@@ -66,29 +102,101 @@ const Page = () => {
             }}
           />
           {/* 기본 인풋.type=file 스타일은 수정할 수 없어 보이지 않게 함. 대신 label로 탐색기(파인더) 열기 */}
-          <label htmlFor="thumbnailUrl" className="p-[8px] ml-[8px]" style={{height: 'fit-content', cursor: 'pointer'}}>
+          <label
+            htmlFor="thumbnail_url"
+            className="p-[8px] ml-[8px]"
+            style={{height: 'fit-content', cursor: 'pointer'}}
+          >
             {blob ? '업로드' : '수정'}
           </label>
         </div>
         <input
           className="flex-grow"
           type="file"
-          id="thumbnailUrl"
-          name="thumbnailUrl"
+          id="thumbnail_url"
+          name="thumbnail_url"
           accept=".jpg, .jpeg, .png"
           onChange={handleChange}
           style={{display: 'none'}}
         />
       </div>
 
-      <div className="flex w-full">
-        <label className="w-[120px]" htmlFor="description">
+      <div className="flex w-full pt-[6px]">
+        <label className="w-[150px]" htmlFor="description">
           투표 설명
         </label>
-        <textarea className="flex-grow" id="description" name="description"></textarea>
+        <textarea className="flex-grow min-h-[300px]" id="description" name="description"></textarea>
       </div>
 
-      <button type="submit">추가하기</button>
+      <div className="flex w-full pt-[6px]">
+        {/* label 태그가 input 요소를 감싸면 텍스트를 클릭했을 때도 선택 된다 */}
+        <div className="w-[150px] pb-[10px]">투표 마감 날짜 여부</div>
+        <div>
+          <label htmlFor="has_end_date">
+            <input
+              className="w-[15px] pr-[6px]"
+              type="checkbox"
+              id="has_end_date"
+              name="has_end_date"
+              checked={hasEndDate}
+              onChange={handleChangeHasEndDate}
+            />
+            있음
+          </label>
+          <label htmlFor="not_has_end_date" className="pl-[8px]">
+            <input
+              className="w-[15px] pr-[6px]"
+              type="checkbox"
+              id="not_has_end_date"
+              name="not_has_end_date"
+              checked={!hasEndDate}
+              onChange={handleChangeHasEndDate}
+            />
+            없음
+          </label>
+        </div>
+      </div>
+
+      <div className="flex w-full pt-[6px]" style={{display: hasEndDate ? '' : 'none'}}>
+        <label className="w-[150px]" htmlFor="end_date">
+          투표 마감 날짜
+        </label>
+
+        <input type="date" disabled={!hasEndDate} id="end_date" name="end_date" min={today}></input>
+      </div>
+
+      {/* 복수 선택 가능 여부 */}
+      <div className="flex w-full pt-[6px]">
+        <div className="w-[150px] pb-[10px]">복수 선택 가능 여부</div>
+        <div>
+          <label htmlFor="allow_multiple_choice">
+            <input
+              className="w-[15px] pr-[6px]"
+              type="checkbox"
+              id="allow_multiple_choice"
+              name="allow_multiple_choice"
+              checked={isAllowMultipleChoice}
+              onChange={handleChangeIsAllowMultipleChoice}
+            />
+            가능
+          </label>
+          <label htmlFor="not_allow_multiple_choice" className="pl-[8px]">
+            <input
+              className="w-[15px] pr-[6px]"
+              type="checkbox"
+              id="not_allow_multiple_choice"
+              name="not_allow_multiple_choice"
+              checked={!isAllowMultipleChoice}
+              onChange={handleChangeIsAllowMultipleChoice}
+            />
+            불가능
+          </label>
+        </div>
+      </div>
+
+      <button type="submit" className="p-[16px] pl-[32px] pr-[32px] rounded-[4px] bg-buttonColor text-white">
+        투표 등록
+      </button>
     </form>
   );
 };
