203b21009fef23c559eb2de34601ef89e6df9cad
ksj12172 <sojeong94@naver.com>
2024-11-28 17:19:59 +0900

    fix: 팝업 배경색 수정, 팝업 띄울 때 헤더도 가리도록 z-index config 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,19 @@ module.exports = {
         lineColor: '#d4d8e9',
         buttonColor: '#0c1325',
         customGray: '#e6e6e6',
+        'popup-bg': '#333333',
+      },
+      opacity: {
+        'popup-bg': '0.25',
       },
       spacing: {
         'header-height': '48px',
         'desktop-header-height': '56px',
         'bottom-nav-height': '60px',
+      },
+      zIndex: {
+        'header-zIndex': 1000,
+        'cover-header': 1001,
       },
     },
   },
