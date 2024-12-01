faa04bc9f92593f3f0648ef7b945bf8dc471d774
ksj12172 <35285113+ksj12172@users.noreply.github.com>
2024-12-01 14:59:28 +0900

    빌드에러 수정(client component도 prerender하기 때문에 react-quill은 안 하게 하기) (#33)
    * fix: 서버 사이드 컴포넌트에서는 route handler api를 호출하지 않는다
    
    * fix: 빌드에러 수정(client component도 prerender하기 때문에 react-quill은 안 하게 하기)

--- src/ui/common/Editor.tsx
+++ src/ui/common/Editor.tsx
@@ -1,11 +1,21 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import {Dispatch, SetStateAction} from 'react';
-import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import {NavBarHeight, isSmallScreenSize} from '../../../const';
 
 const toolbarHeight = 44;
+const titleYMargin = 16;
+
+/**
+ * 빌드시 client component도 prerender 하려다가 에러난다
+ * dynamic import로 해결하자
+ */
+const QuillNoSSRWrapper = dynamic(() => import('react-quill'), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
 
 /**
  * react-quill github:
@@ -18,8 +28,9 @@ const toolbarHeight = 44;
  */
 
 const Editor = ({content, setContent}: {content: string; setContent: Dispatch<SetStateAction<string>>}) => {
-  const HeaderHeight = isSmallScreenSize() ? 48 : 56;
-  const editorContentHeight = window.innerHeight - HeaderHeight - NavBarHeight - toolbarHeight;
+  const HeaderHeight = window && isSmallScreenSize() ? 48 : 56;
+
+  const editorContentHeight = window.innerHeight - HeaderHeight - NavBarHeight - toolbarHeight - titleYMargin;
   const modules = {
     toolbar: [
       [{header: [1, 2, 3, 4, 5, false]}],
@@ -45,7 +56,7 @@ const Editor = ({content, setContent}: {content: string; setContent: Dispatch<Se
   ];
 
   return (
-    <ReactQuill
+    <QuillNoSSRWrapper
       theme="snow"
       value={content}
       onChange={setContent}
